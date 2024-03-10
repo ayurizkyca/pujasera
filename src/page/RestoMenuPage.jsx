43b1a014bab0627454acf3c4f99bdbcd43ec28ea
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-10 18:20:40 +0700

    fix : detai resto page

--- src/page/RestoMenuPage.jsx
+++ src/page/RestoMenuPage.jsx
@@ -3,10 +3,18 @@ import CardMenu from '../component/CardMenu.jsx'
 import { restoData } from '../../public/data/restoData.js'
 import { useParams } from 'react-router-dom'
 import { Typography } from 'antd'
+import { useDispatch } from 'react-redux'
+import { cartActions } from '../redux/cart.js'
+
 
 const RestoMenuPage = () => {
     const { id } = useParams();
     const resto = restoData.find((r) => r.id === id);
+    const dispatch = useDispatch();
+
+    const addToCartHandler = (idResto, namaResto, id, name, price) => {
+        dispatch(cartActions.addMenuItem({ idResto, namaResto, idMenu: id, namaMenu: name, harga: price, qty: 1 }));
+    };
 
     return (
         <>
@@ -20,12 +28,13 @@ const RestoMenuPage = () => {
                         imageUrl={card.imageUrl}
                         id={card.id}
                         price={card.price}
+                        onClick={() => addToCartHandler(resto.id, resto.title, card.id, card.name, card.price)}
                     />
                 ))}
             </div>
         </>
+    );
+};
 
-    )
-}
 
 export default RestoMenuPage
