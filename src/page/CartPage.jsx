5056f07dd21ed049293c99a37dddda4f288c7492
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-11 05:36:06 +0700

    fix : add cust

--- src/page/CartPage.jsx
+++ src/page/CartPage.jsx
@@ -9,15 +9,20 @@ const CartPage = () => {
     const menuItem = useSelector(state => state.cart.menuItem);
     const total = useSelector((state) => state.cart.total)
     const cartItemCount = useSelector((state) => state.cart.menuItem.reduce((acc, resto) => acc + resto.menu.length, 0));
-
+    const customer = useSelector((state) => state.cart.customer)
+    const meja = useSelector((state) => state.cart.meja);
 
     return (
-        <div>
-            <div className='flex h-[100px] items-center justify-between p-[30px] shadow'>
+        <div className='h-screen flex flex-col'>
+            <div className='flex h-24 items-center justify-between p-8 shadow'>
                 <img src={logo} alt="logo" />
                 <h1 className='text-2xl'>Keranjang Belanja</h1>
             </div>
-            <ul>
+            <div className='px-8 py-3'>
+                <h1 className='text-xl font-medium'>Customer : {customer}</h1>
+                <h1 className='text-xl font-medium'>Meja : {meja}</h1>
+            </div>
+            <ul className=' flex-grow'>
                 {menuItem.map(resto => (
                     <CardCart
                         key={resto.idResto}
@@ -35,11 +40,10 @@ const CartPage = () => {
                     </CardCart>
                 ))}
             </ul>
-            <div className='sticky bottom-0 left-0 w-full bg-white border-t border-primary p-4 flex justify-between items-center'>
+            <div className='sticky bottom-0 bg-white border-t border-primary p-4 flex justify-between items-center'>
                 <h3 className=''>Total ({cartItemCount}) produk : <span className='font-bold text-2xl text-primary'>Rp. {total}</span></h3>
                 <ButtonBasic className="" title={"checkout"}/>
             </div>
-
         </div>
     );
 };
