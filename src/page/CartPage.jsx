3552e68b5bb1d91a8a01489508c5c5a0d9a5a8d0
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-11 02:58:56 +0700

    modify : cart page

--- src/page/CartPage.jsx
+++ src/page/CartPage.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import CardCart from '../component/CardCart';
+import ListMenuCart from '../component/ListMenuCart';
 
-export default function CartPage() {
-  return (
-    <div>CartPage</div>
-  )
-}
+const CartMenuList = () => {
+    const menuItem = useSelector(state => state.cart.menuItem);
+    const total = useSelector((state) => state.cart.total)
+
+    return (
+        <div>
+            <h2>Daftar Menu</h2>
+            <ul>
+                {menuItem.map(resto => (
+                    <CardCart 
+                        key={resto.idResto}
+                        namaResto={resto.namaResto}
+                    >
+                        {resto.menu.map(menu => (
+                            <ListMenuCart
+                                key={menu.idMenu}
+                                namaMenu={menu.namaMenu}
+                                harga={menu.harga}
+                                qty={menu.qty}
+                                subTotal={menu.qty * menu.harga}
+                            />
+                        ))}
+                    </CardCart>
+                ))}
+            </ul>
+            <h3>Total: Rp. {total}</h3>
+        </div>
+    );
+};
+
+export default CartMenuList;
