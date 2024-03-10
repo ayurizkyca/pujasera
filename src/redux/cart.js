7f2f2432e94588e93e153b6b98f0929ba0938ad2
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-11 00:03:35 +0700

    modify : cart reducers

--- src/redux/cart.js
+++ src/redux/cart.js
@@ -3,8 +3,10 @@ import { createSlice } from "@reduxjs/toolkit"
 const initialCartState = {
     customer: "",
     meja: "",
-    // cart: [],
-    menuItem : []
+    isUserEmpty: true,
+    menuItem: [],
+    subtotal: 0,
+    total: 0,
 }
 
 const cartSlice = createSlice({
@@ -13,8 +15,9 @@ const cartSlice = createSlice({
     reducers: {
         addCustomer(state, action) {
             // const { customer, meja } = action.payload;
-            state.customer = action.payload.customer;
-            state.meja = action.payload.meja;
+            state.customer = action.payload.customer
+            state.meja = action.payload.meja
+            isUserEmpty = false
             // const findCustomer = state.idCust;
             // if(findCustomer < 0){
             //     state.name = name,
@@ -23,63 +26,68 @@ const cartSlice = createSlice({
 
             // }
         },
-        // addMenuToCart(state, action) {
-        //     const newItem = action.payload;
-        //     const existingResto = state.cart.find((resto) => resto.id === newItem.id);
-        //     if (!existingResto) {
-        //         state.cart.push({
-        //             id: newItem.restoId,
-        //             name: newItem.namaResto,
-        //             menu: [{
-        //                 id: newItem.id,
-        //                 name: newItem.name,
-        //                 price: newItem.price,
-        //                 quantity: 1,
-        //             }]
-        //         })
-        //         console.log(existingResto);
-        //     } else {
-        //         const existingItem = existingResto.cart.find((item) => item.id === newItem.id);
-        //         if (!existingItem) {
-        //             existingResto.cart.push({
-        //                 id: newItem.id,
-        //                 name: newItem.name,
-        //                 price: newItem.price,
-        //                 quantity: 1,
-        //             });
+       
+        // addMenuItem(state, action) {
+        //     const { idResto, namaResto, idMenu, namaMenu, harga, qty } = action.payload;
+        
+        //     // Cari restoran dalam array menuItem berdasarkan idResto
+        //     const existingRestoIndex = state.menuItem.findIndex(item => item.idResto === idResto);
+        
+        //     if (existingRestoIndex !== -1) {
+        //         // Restoran sudah ada, tambahkan menu ke dalam restoran yang sudah ada
+        //         const existingResto = state.menuItem[existingRestoIndex];
+        //         const existingMenuIndex = existingResto.menu.findIndex(menu => menu.idMenu === idMenu);
+        
+        //         if (existingMenuIndex !== -1) {
+        //             // Menu sudah ada, update jumlah qty
+        //             existingResto.menu[existingMenuIndex].qty += qty;
+        //             console.log("tambah qty")
         //         } else {
-        //             existingItem.quantity++;
+        //             // Menu belum ada, tambahkan menu baru
+        //             existingResto.menu.push({ idMenu, namaMenu, harga, qty });
+        //             console.log("tambah menu")
         //         }
-        //         console.log("ini else yang jalan")
+        //     } else {
+        //         // Restoran belum ada, tambahkan restoran baru beserta menu
+        //         const newResto = {
+        //             idResto,
+        //             namaResto,
+        //             menu: [{ idMenu, namaMenu, harga, qty }],
+        //         };
+        //         state.menuItem.push(newResto);
+        //         console.log("tambah resto dan menu")
         //     }
         // },
+
         addMenuItem(state, action) {
             const { idResto, namaResto, idMenu, namaMenu, harga, qty } = action.payload;
-            // Cek apakah restoran sudah ada di keranjang belanja
+
             const existingRestoIndex = state.menuItem.findIndex(item => item.idResto === idResto);
             if (existingRestoIndex !== -1) {
-                // Restoran sudah ada, tambahkan menu ke dalam restoran yang sudah ada
                 const existingResto = state.menuItem[existingRestoIndex];
                 const existingMenuIndex = existingResto.menu.findIndex(menu => menu.idMenu === idMenu);
+
                 if (existingMenuIndex !== -1) {
-                    // Menu sudah ada, update jumlah qty
                     existingResto.menu[existingMenuIndex].qty += qty;
-                    console.log("tambah qty")
+                    state.subtotal += harga * qty;
                 } else {
-                    // Menu belum ada, tambahkan menu baru
                     existingResto.menu.push({ idMenu, namaMenu, harga, qty });
-                    console.log("tambah menu")
+                    state.subtotal += harga * qty;
                 }
             } else {
-                // Restoran belum ada, tambahkan restoran baru beserta menu
-                state.menuItem.push({
+                const newResto = {
                     idResto,
                     namaResto,
                     menu: [{ idMenu, namaMenu, harga, qty }],
-                });
-                console.log("tambah resto dan menu")
+                };
+                state.menuItem.push(newResto);
+                state.subtotal += harga * qty;
             }
+            state.total = state.subtotal; // Set total sama dengan subtotal saat menambah item baru
         },
+        
+        
+        
         clearCart(state) {
 
         }
