465311bba04e1b000429bfb006ac87a31057042d
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-11 00:13:57 +0700

    modify : table history

--- src/component/Table.jsx
+++ src/component/Table.jsx
@@ -2,24 +2,19 @@ import React from 'react';
 import { Table } from 'antd';
 const columns = [
   {
-    title: 'Nomor Pesanan',
-    dataIndex: 'nomor_pesanan',
-    key: 'nomor_pesanan',
+    title: 'Customer',
+    dataIndex: 'customer',
+    key: 'customer',
   },
   {
-    title: 'Resto',
-    dataIndex: 'resto',
-    key: 'resto',
+    title: 'Meja',
+    dataIndex: 'meja',
+    key: 'meja',
   },
   {
-    title: 'Menu',
-    dataIndex: 'menu',
-    key: 'menu',
-  },
-  {
-    title: 'Jumlah',
-    dataIndex: 'jumlah',
-    key: 'jumlah',
+    title: 'Restaurants',
+    dataIndex: 'restaurant',
+    key: 'restaurant',
   },
   {
     title: 'Total',
@@ -36,31 +31,28 @@ const columns = [
 const data = [
   {
     key: 1,
-    nomor_pesanan: '001',
-    resto: 'Pizza Palace',
-    menu: 'Hawaiian Pizza',
-    jumlah : 31,
+    customer: '001',
+    restaurant: 'Hawaiian Pizza',
+    meja : 31,
     total : 'Rp.20.000',
-    note: 'Hawaiian Pizza without Pineapple',
+    detail: 'Hawaiian Pizza without Pineapple',
   },
   {
     key: 2,
-    nomor_pesanan: '002',
-    resto: 'Burger Barn',
-    menu: 'Classic Cheesburger',
-    jumlah : 3,
+    customer: '002',
+    restaurant: 'Classic Cheesburger',
+    meja : 3,
     total : 'Rp.80.000',
-    note: 'Add more cheese please',
+    detail: 'Add more cheese please',
   },
   {
     key: 3,
     name: 'Not Expandable',
-    nomor_pesanan: '003',
-    resto: 'Pizza Palace',
-    menu: 'Vegetarian Pizza',
-    jumlah : 31,
+    customer: '003',
+    restaurant: 'Vegetarian Pizza',
+    meja : 31,
     total : 'Rp.20.000',
-    note: '',
+    detail: '',
   },
 ];
 const TableCustom = () => (
@@ -73,7 +65,7 @@ const TableCustom = () => (
             margin: 0,
           }}
         >
-          {record.note}
+          {record.detail}
         </p>
       ),
       rowExpandable: (record) => record.name !== 'Not Expandable',
