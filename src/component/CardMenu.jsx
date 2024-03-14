0f7404849fecd2ced97b3d4c47f52025dc38df11
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-14 09:50:14 +0700

    modify : card menu

--- src/component/CardMenu.jsx
+++ src/component/CardMenu.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Card, Typography } from 'antd';
 const { Meta } = Card;
 import ButtonBasic from './ButtonBasic';
+import { formatRupiah } from '../util/format';
 
 const CardMenu = ({ name, description, imageUrl, price, onClick }) => {
  
@@ -17,7 +18,7 @@ const CardMenu = ({ name, description, imageUrl, price, onClick }) => {
         <div className='flex flex-col justify-between h-full'>
           <div>
             <Meta title={name} description={<div className="line-clamp-2">{description}</div>}  />
-            <Typography.Title level={4} className='mt-5 font-bold text-end '>Rp. {price}</Typography.Title>
+            <Typography.Title level={4} className='mt-5 font-bold text-end '>{formatRupiah(price)}</Typography.Title>
           </div>
           <ButtonBasic title={"add to cart"} onClick={onClick} />
         </div>
