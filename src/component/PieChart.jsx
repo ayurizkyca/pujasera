acdb86d14bc6e2042f50f7b283e74b9787c2b35a
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-14 09:51:27 +0700

    modify : pie chart

--- src/component/PieChart.jsx
+++ src/component/PieChart.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Pie } from 'react-chartjs-2';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 const PieChart = ({data}) => {
   return <Pie data={data} />;
