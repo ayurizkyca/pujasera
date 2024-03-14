d96ffab201c58aeb1f764b165e79d1eda3212f56
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-14 09:50:40 +0700

    modify : navbar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -9,7 +9,7 @@ import {
 } from '@ant-design/icons';
 import { Drawer, Button, Form, Input, Badge, message, Modal } from 'antd';
 import { cartActions } from '../redux/cart'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { ROUTES } from '../constant/routesConstant'
 
 
@@ -17,6 +17,8 @@ export default function Navbar() {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const customerExist = useSelector((state) => state.cart.customer)
+    const tableExist = useSelector((state) => state.cart.meja)
 
     //modal
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -35,16 +37,11 @@ export default function Navbar() {
     const open = useSelector((state) => state.cart.isDrawerOpen);
     const isCustEmpty = useSelector((state) => state.cart.isCustEmpty)
     const handleDrawerOpen = () => {
-        if (isCustEmpty === false) {
-            message.error("you've got a table")
-        } else {
-            dispatch(cartActions.toggleDrawer(false));
-        }
+        dispatch(cartActions.toggleDrawer(false));
     }
     const handleDrawerClose = () => {
         dispatch(cartActions.toggleDrawer(true));
     }
-
     const [customerData, setCustomerData] = useState({
         customer: "",
         meja: ""
@@ -76,14 +73,16 @@ export default function Navbar() {
         <>
             <div className='flex justify-between border h-[80px] bg-white shadow-lg shadow-slate-200 sticky top-0 z-50'>
                 <div className='flex justify-between w-full items-center mx-10'>
-                    <img src={ImagePujasera} alt="image-pujasera" className='w-[200px]' />
+                    <Link to={ROUTES.PORTAL_RESTO}>
+                        <img src={ImagePujasera} alt="image-pujasera" className='w-[200px]' />
+                    </Link>
                     <div>
                         <div className='flex gap-5 items-center'>
                             <Badge className='' count={cartItemCount}>
                                 <ShoppingCartOutlined className='w-5' onClick={showCart} />
                             </Badge>
                             <div className='flex gap-2'>
-                                <p>{username}</p>
+                                <p className='hidden md:flex'>{username}</p>
                                 <UserOutlined onClick={handleDrawerOpen} />
                             </div>
                             <ButtonBasic title={"Logout"} onClick={showModal} />
@@ -91,17 +90,17 @@ export default function Navbar() {
                     </div>
                 </div>
             </div>
-            <Drawer title="Input Customer Data" onClose={handleDrawerClose} open={open}>
-                <Form
+            <Drawer title={isCustEmpty ? "Input Customer Detail" : "Customer Detail"} onClose={handleDrawerClose} open={open}>
+                {/* <Form
                     name="basic"
                     onFinish={onFinish}
-                    // onFinishFailed={onFinishFailed}
                     autoComplete="off"
                     layout='vertical'
                 >
                     <Form.Item
                         label="Customer"
                         name="customer"
+                        placeholder="eg.ayu"
                         rules={[
                             {
                                 required: true,
@@ -109,7 +108,7 @@ export default function Navbar() {
                             },
                         ]}
                     >
-                        <Input onChange={onChange} value={customerData.customer} />
+                        <Input placeholder="eg. ayu" onChange={onChange} value={customerData.customer} />
                     </Form.Item>
 
                     <Form.Item
@@ -122,7 +121,7 @@ export default function Navbar() {
                             },
                         ]}
                     >
-                        <Input onChange={onChange} value={customerData.meja} />
+                        <Input placeholder="eg. 10" onChange={onChange} value={customerData.meja} />
                     </Form.Item>
 
                     <Form.Item
@@ -135,7 +134,58 @@ export default function Navbar() {
                             Get Table
                         </Button>
                     </Form.Item>
-                </Form>
+                </Form> */}
+                {isCustEmpty ? (
+                    <Form
+                        name="basic"
+                        onFinish={onFinish}
+                        autoComplete="off"
+                        layout='vertical'
+                    >
+                        <Form.Item
+                            label="Customer"
+                            name="customer"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your name',
+                                },
+                            ]}
+                        >
+                            <Input onChange={onChange} value={customerData.customer} />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Table"
+                            name="meja"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input table',
+                                },
+                            ]}
+                        >
+                            <Input onChange={onChange} value={customerData.meja} />
+                        </Form.Item>
+
+                        <Form.Item
+                            wrapperCol={{
+                                offset: 8,
+                                span: 16,
+                            }}
+                        >
+                            <Button type="primary" htmlType="submit" className='bg-primary'>
+                                Get Table
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                ) : (
+                    <div>
+                        <p><span className='font-bold'>Customer :</span> {customerExist}</p>
+                        <p><span className='font-bold'>Table :</span> {tableExist}</p>
+                    </div>
+                )}
+
             </Drawer>
             <Modal title="Logout" open={isModalOpen} onOk={logoutClick} onCancel={handleCancel} okType='danger'>
                 <p>Are you sure you want to exit?</p>
