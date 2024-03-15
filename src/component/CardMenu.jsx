a22add10b85eafac34bff4f6745b01b83eadd26f
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-15 15:22:06 +0700

    modify : style card

--- src/component/CardMenu.jsx
+++ src/component/CardMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Typography, Tooltip } from 'antd';
+import { Card, Typography, Tooltip, Button } from 'antd';
 const { Meta } = Card;
 import ButtonBasic from './ButtonBasic';
 import { formatRupiah } from '../util/format';
@@ -16,16 +16,17 @@ const CardMenu = ({ name, description, imageUrl, price, onClick }) => {
         cover={<img className='h-[200px] object-cover' alt="card-image" src={imageUrl} />}
       >
         <div className='flex flex-col justify-between h-full'>
-          <div>
+          <div className='space-y-[2vh]'>
             <Meta title={name} description={
               <Tooltip title={description}>
                 <div className="line-clamp-2">{description}</div>
               </Tooltip>
             }
             />
-            <Typography.Title level={4} className='mt-5 font-bold text-end '>{formatRupiah(price)}</Typography.Title>
+            <h1 className='mt-5 font-bold text-xl text-end text-primary'>{formatRupiah(price)}</h1>
+            <ButtonBasic title={"add to cart"} onClick={onClick} textColor={'primary'} color={'secondary'} />
+            {/* <Button className="bg-secondary text-primary w-full font-bold border-none hover:bg-primary">add to chart</Button> */}
           </div>
-          <ButtonBasic title={"add to cart"} onClick={onClick} />
         </div>
       </Card>
     </div>
