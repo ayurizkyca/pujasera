83a55c2eee2046923cbe5e3eeb48452f13d4d328
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-19 11:40:41 +0700

    modify : add menu

--- src/page/RestoMenuPage.jsx
+++ src/page/RestoMenuPage.jsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react'
 import CardMenu from '../component/CardMenu.jsx'
 import { restoData } from '../../public/data/restoData.js'
 import { useParams } from 'react-router-dom'
-import { Typography, Modal, Form, Input } from 'antd'
+import { Typography, Modal, Form, Input, message, Button } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { cartActions } from '../redux/cart.js'
-import { message } from 'antd/es/index.js'
 import ButtonBasic from '../component/ButtonBasic.jsx'
+import UploadCustom from '../component/UploadCustom.jsx'
+import { menuActions } from '../redux/menu.js'
 
 
 const RestoMenuPage = () => {
     const { id } = useParams();
     const resto = restoData.find((r) => r.id === id);
+    const restaurant = useSelector((state) => state.menu.resto);
     const dispatch = useDispatch();
     const isCustEmpty = useSelector((state) => state.cart.isCustEmpty)
 
@@ -28,6 +30,56 @@ const RestoMenuPage = () => {
     const [isAddMenuOpen, setIsAddMenuOpen] = useState(false);
     const showAddMenu = () => {
         setIsAddMenuOpen(true);
+    };
+
+    // const handleAddMenu = (values) => {
+    //     dispatch(
+    //         menuActions.addMenu({
+    //             idResto: id,
+    //             namaResto: resto.title,
+    //             id: Date.now().toString(), 
+    //             name: values.name,
+    //             description: values.description,
+    //             imageUrl: values.imageUrl,
+    //             price: parseFloat(values.price),
+    //             stock: parseInt(values.stock),
+    //             qty: 1,
+    //         })
+    //     );
+    //     setIsAddMenuOpen(false);
+    //     message.success('Menu added successfully!');
+    // };
+
+    const [menuData, setMenuData] = useState({
+        name: "",
+        description: "",
+        imageUrl: "",
+        price: "",
+        stock: ""
+    })
+
+    const onChange = (event) => {
+        setMenuData({
+            ...menuData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    // const handleAddMenu = (values) => {
+    //     dispatch(menuActions.addMenu(values));
+    //     message.success("succesfully add menu")
+    //     console.log(values)
+    // }
+    const handleAddMenu = (values) => {
+        dispatch(menuActions.addMenu({
+            idResto: id, // Anda mungkin perlu menyediakan ID restoran secara dinamis
+            // menu: { ...menuData }
+            menu: values
+
+        }));
+        message.success("successfully added menu");
+        setIsAddMenuOpen(false);
+        console.log(values)
     };
 
     return (
@@ -53,42 +105,67 @@ const RestoMenuPage = () => {
                     />
                 ))}
             </div>
+            {/* <div className='flex flex-wrap gap-2'>
+                {restaurant?.menus.map(card => (
+                    <CardMenu
+                        key={card.id}
+                        name={card.name}
+                        description={card.description}
+                        imageUrl={card.imageUrl}
+                        id={card.id}
+                        price={card.price}
+                        stock={card.stock}
+                        onClick={() => addToCartHandler(resto.id, resto.title, card.id, card.name, card.price)}
+                    />
+                ))}
+            </div> */}
             <Modal title="Add Menu Resto" open={isAddMenuOpen} okType='danger' onCancel={() => setIsAddMenuOpen(false)}>
                 <Form className='m-2 mt-5'
                     name="basic"
-                    // onFinish={onFinish}
+                    onFinish={handleAddMenu}
                     autoComplete="off"
                     layout='vertical'
                 >
                     <Form.Item
-                        label="Menu Name"
+                        label="Name"
                         name="name"
                     >
-                        <Input onChange={""} value={""} required />
+                        <Input onChange={onChange} value={menuData.name} required />
                     </Form.Item>
                     <Form.Item
                         label="Description"
                         name="description"
                     >
-                        <Input onChange={""} value={""} required />
+                        <Input onChange={onChange} value={menuData.description} required />
                     </Form.Item>
                     <Form.Item
                         label="Image"
                         name="imageUrl"
                     >
-                        <Input onChange={""} value={""} required />
+                        <Input onChange={onChange} value={menuData.imageUrl} required />
+                        {/* <UploadCustom /> */}
                     </Form.Item>
                     <Form.Item
                         label="Price"
                         name="price"
                     >
-                        <Input onChange={""} value={""} required type='number' />
+                        <Input onChange={onChange} value={menuData.price} required type='number' />
                     </Form.Item>
                     <Form.Item
                         label="Stock"
                         name="stock"
                     >
-                        <Input onChange={""} value={""} required type='number' />
+                        <Input onChange={onChange} value={menuData.stock} required type='number' />
+                    </Form.Item>
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit" className='bg-primary'>
+                            Add Menu
+                        </Button>
                     </Form.Item>
                 </Form>
             </Modal>
