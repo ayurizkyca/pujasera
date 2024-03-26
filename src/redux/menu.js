599bbee53a3a82ee94444a65b107886d620a16c8
Ayu Rizkyca Awalia <ayu.rizkyca@nexsoft.co.id>
2024-03-26 14:18:46 +0700

    update : menu slice

--- src/redux/menu.js
+++ src/redux/menu.js
@@ -9,33 +9,10 @@ const menuSlice = createSlice({
   name: "menu",
   initialState: initialMenuState,
   reducers: {
-    addMenu(state, action) {
-      const { idResto, menu } = action.payload;
-      const restoIndex = state.resto.findIndex(resto => resto.id === idResto);
-      if (restoIndex !== -1) {
-        state.resto[restoIndex].menus.push(menu);
-      }
+    updateMenu: (state, action) => {
+      state.resto = action.payload;
     },
-    deleteMenu(state, action) {
-      const { idResto, idMenu } = action.payload;
-      const restoIndex = state.resto.findIndex(resto => resto.id === idResto);
-      if (restoIndex !== -1) {
-        const menuIndex = state.resto[restoIndex].menus.findIndex(menu => menu.id === idMenu);
-        if (menuIndex !== -1) {
-          state.resto[restoIndex].menus.splice(menuIndex, 1);
-        }
-      }
-    },
-    editMenu(state, action) {
-      const { idResto, idMenu, menu } = action.payload;
-      const restoIndex = state.resto.findIndex(resto => resto.id === idResto);
-      if (restoIndex !== -1) {
-        const menuIndex = state.resto[restoIndex].menus.findIndex(menu => menu.id === idMenu);
-        if (menuIndex !== -1) {
-          state.resto[restoIndex].menus[menuIndex] = menu;
-        }
-      }
-    },
+
     updateStock(state, action) {
       console.log("masuk redux")
       const { idResto, idMenu, stock } = action.payload;
